ba096bcb57f58344200c8b3c884cbd9dcdb3572a
Nipunn Koorapati <nipunn@convex.dev>
2024-10-16 17:41:54 -0700

    Fix python client tests. (#30801)
    Fix tests to include version param.
    
    GitOrigin-RevId: 13a7a150c641049c4140a99ef2c318afc15f8480

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -1,9 +1,9 @@
 from _convex import PyConvexClient
-from convex import ConvexClient
+from convex import ConvexClient, __version__
 
 
 def test_rust_instantiation() -> None:
-    PyConvexClient("https://made-up-animal.convex.cloud")
+    PyConvexClient("https://made-up-animal.convex.cloud", __version__)
 
 
 def test_instantiation() -> None:
